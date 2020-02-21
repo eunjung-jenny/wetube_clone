1a7738e66056b7d4556f340460ca0249f7861efc
jenny.kr <jenny.it.kr@gmail.com>
2020-02-21 21:24:34 +0900

    Update routes and Router

--- app.js
+++ app.js
@@ -4,30 +4,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => {
-  res.send("Hello from home!");
-};
-
-const handleProfile = (req, res) => {
-  res.send("You are on my profile");
-};
-
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome); // main URL
-
-app.get("/profile", handleProfile);
-
 // /user route 접속시 userRouter 전체를 사용함
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 // app 객체를 내보내기
 export default app;
